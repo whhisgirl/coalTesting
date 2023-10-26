faba3ffd546c8280aa37d02517400de004e38681
Ro1eee <910222523@qq.com>
2023-10-26 09:39:26 +0800

    第四次提交

--- UI/src/views/test/analyse/analyse.vue
+++ UI/src/views/test/analyse/analyse.vue
@@ -1,98 +1,116 @@
 <template>
-
   <body>
   <header>
     <h1>煤炭指标分析</h1>
   </header>
   <div>
-    <div class="divBox">
-      <div class="top">
-        <el-table
-          :data="tableData"
-          stripe
-          style="width: auto">
-          <el-table-column
-            prop="number"
-            label="序号"
-            width="100">
-          </el-table-column>
-          <el-table-column
-            prop="id"
-            label="煤炭编号"
-            width="100">
-          </el-table-column>
-          <el-table-column
-            prop="shuifen1"
-            label="第一次检测水分"
-            width="120">
-          </el-table-column>
-          <el-table-column
-            prop="shuifen2"
-            label="第二次检测水分"
-            width="120">
-          </el-table-column>
-          <el-table-column
-            prop="huifen1"
-            label="第一次检测灰分"
-            width="120">
-          </el-table-column>
-          <el-table-column
-            prop="huifen2"
-            label="第二次检测灰分"
-            width="120">
-          </el-table-column>
-          <el-table-column
-            prop="midu1"
-            label="第一次检测密度"
-            width="120">
-          </el-table-column>
-          <el-table-column
-            prop="midu2"
-            label="第二次检测密度"
-            width="120">
-          </el-table-column>
-          <el-table-column
-            prop="huifafen1"
-            label="第一次检测挥发分"
-            width="130">
-          </el-table-column>
-          <el-table-column
-            prop="huifafen2"
-            label="第二次检测挥发分"
-            width="130">
-          </el-table-column>
-          <el-table-column
-            prop="dianzulv1"
-            label="第一次检测电阻率"
-            width="130">
-          </el-table-column>
-          <el-table-column
-            prop="dianzulv2"
-            label="第二次检测电阻率"
-            width="130">
-          </el-table-column>
-          <el-table-column
-            prop="jidi1"
-            label="第一次检测基地位发热量"
+    <el-breadcrumb separator="/">
+      <div class="el-card__body">
+      <div class="divBox">
+        <div class="top">
+          <el-table
+            :data="tableData"
+            height="500"
+            border
+            style="width: 100%">
+            <el-table-column
+              prop="number"
+              label="序号"
+              width="100">
+            </el-table-column>
+            <el-table-column
+              prop="id"
+              label="煤炭编号"
+              width="100">
+            </el-table-column>
+            <el-table-column
+              prop="shuifen1"
+              label="第一次检测水分"
+              width="120">
+            </el-table-column>
+            <el-table-column
+              prop="shuifen2"
+              label="第二次检测水分"
+              width="120">
+            </el-table-column>
+            <el-table-column
+              prop="huifen1"
+              label="第一次检测灰分"
+              width="120">
+            </el-table-column>
+            <el-table-column
+              prop="huifen2"
+              label="第二次检测灰分"
+              width="120">
+            </el-table-column>
+            <el-table-column
+              prop="midu1"
+              label="第一次检测密度"
+              width="120">
+            </el-table-column>
+            <el-table-column
+              prop="midu2"
+              label="第二次检测密度"
+              width="120">
+            </el-table-column>
+            <el-table-column
+              prop="huifafen1"
+              label="第一次检测挥发分"
+              width="130">
+            </el-table-column>
+            <el-table-column
+              prop="huifafen2"
+              label="第二次检测挥发分"
+              width="130">
+            </el-table-column>
+            <el-table-column
+              prop="dianzulv1"
+              label="第一次检测电阻率"
+              width="130">
+            </el-table-column>
+            <el-table-column
+              prop="dianzulv2"
+              label="第二次检测电阻率"
+              width="130">
+            </el-table-column>
+            <el-table-column
+              prop="jidi1"
+              label="第一次检测基地位发热量"
+              width="190">
+            </el-table-column><el-table-column
+            prop="jidi2"
+            label="第二次检测基地位发热量"
             width="190">
-          </el-table-column><el-table-column
-          prop="jidi2"
-          label="第二次检测基地位发热量"
-          width="190">
-        </el-table-column>
-        </el-table></div>
-      <div class="bottom">
-        <div  ref="chart" style="width: 1000px;height:500px">
+          </el-table-column>
+            <div class="block">
+              <span class="demonstration">完整功能</span>
+              <el-pagination
+                @size-change="handleSizeChange"
+                @current-change="handleCurrentChange"
+                :current-page="currentPage4"
+                :page-sizes="[100, 200, 300, 400]"
+                :page-size="100"
+                layout="total, sizes, prev, pager, next, jumper"
+                :total="400">
+              </el-pagination>
+            </div>
+          </el-table></div>
+        <div class="bottom">
+          <el-col span="22">
+          <div  ref="chart" style="width: 100%;height:500px">
+          </div>
+          </el-col>
         </div>
       </div>
-    </div>
+      </div>
+    </el-breadcrumb>
   </div>
-
   </body>
 </template>
 
 <script>
 import * as echarts from 'echarts';
+import {listIndicators} from "@/api/system/indicators"
 
 export default {
   name: "analyse",
@@ -203,13 +221,88 @@ export default {
         dianzulv2: '4',
         jidi1: '29.27',
         jidi2: '29.27',
-      },]
+      },{
+        number: '1',
+        id: '2023',
+        shuifen1: '8.84',
+        shuifen2: '8.84',
+        huifen1: '22.61',
+        huifen2: '22.61',
+        midu1: '1.4',
+        midu2: '1.4',
+        huifafen1: '28.31',
+        huifafen2: '28.31',
+        dianzulv1: '4',
+        dianzulv2: '4',
+        jidi1: '29.27',
+        jidi2: '29.27',
+      },{
+        number: '1',
+        id: '2023',
+        shuifen1: '8.84',
+        shuifen2: '8.84',
+        huifen1: '22.61',
+        huifen2: '22.61',
+        midu1: '1.4',
+        midu2: '1.4',
+        huifafen1: '28.31',
+        huifafen2: '28.31',
+        dianzulv1: '4',
+        dianzulv2: '4',
+        jidi1: '29.27',
+        jidi2: '29.27',
+      },{
+        number: '1',
+        id: '2023',
+        shuifen1: '8.84',
+        shuifen2: '8.84',
+        huifen1: '22.61',
+        huifen2: '22.61',
+        midu1: '1.4',
+        midu2: '1.4',
+        huifafen1: '28.31',
+        huifafen2: '28.31',
+        dianzulv1: '4',
+        dianzulv2: '4',
+        jidi1: '29.27',
+        jidi2: '29.27',
+      },{
+        number: '1',
+        id: '2023',
+        shuifen1: '8.84',
+        shuifen2: '8.84',
+        huifen1: '22.61',
+        huifen2: '22.61',
+        midu1: '1.4',
+        midu2: '1.4',
+        huifafen1: '28.31',
+        huifafen2: '28.31',
+        dianzulv1: '4',
+        dianzulv2: '4',
+        jidi1: '29.27',
+        jidi2: '29.27',
+      },{
+        number: '1',
+        id: '2023',
+        shuifen1: '8.84',
+        shuifen2: '8.84',
+        huifen1: '22.61',
+        huifen2: '22.61',
+        midu1: '1.4',
+        midu2: '1.4',
+        huifafen1: '28.31',
+        huifafen2: '28.31',
+        dianzulv1: '4',
+        dianzulv2: '4',
+        jidi1: '29.27',
+        jidi2: '29.27',
+      }]
     }
   },
   mounted() {
-    this.getEchartData()
+    this.getEchartData();
   },
-  methods:{
+ methods:{
     getEchartData(){
       const chart = this.$refs.chart
       if(chart){
@@ -236,10 +329,12 @@ export default {
             }
           ],
           xAxis:{
+            show:false,
             type:'category',
             axisPointer: {
               type: 'shadow'
             },
+
             axisTick:{
               alignWithLabel: true
             },
@@ -274,6 +369,10 @@ export default {
             }
           ]
         };
+        //echarts图跟随变化
+        window.addEventListener("resize", function () {
+          myChart.resize();
+        });
         option && myChart.setOption(option);
       }
     }
@@ -298,17 +397,17 @@ header h1 {
   padding: 0;
 }
 .divBox{
-  height: 2000px;
+  height: 1200px;
 }
 .top{
   position: absolute;
   float:left;
-  width:2000px;
+  width:1825px;
   height: 100%;
 }
 .bottom{
   position: absolute;
-  top:600px;
+  top:650px;
   height: 100%;
   width: 2000px;
 }
