1b758dcbb73b3b14257d733ac9e06d0751052ab7
Ro1eee <910222523@qq.com>
2023-10-26 17:04:11 +0800

    第六次提交

--- UI/src/views/test/analyse/analyse.vue
+++ UI/src/views/test/analyse/analyse.vue
@@ -1,13 +1,27 @@
 <template>
-  <body>
-  <header>
-    <h1>煤炭指标分析</h1>
-  </header>
+
+<el-card>
+    <div data-v-41eafe88="" style="width: 100%; background: rgb(210, 233, 255); border-radius: 10px;">
+      <p data-v-41eafe88="" style="font-family: Arial;
+      font-size: 24px; font-weight: 600;
+       display: inline-block;
+       margin-left: 20px;">
+        煤炭分析记录 </p>
+    </div>
+
   <div>
     <el-breadcrumb separator="/">
       <div class="el-card__body">
       <div class="divBox">
         <div class="top">
+          <div class="demo-input-suffix">
+            <el-input
+              style="width: 150px"
+              placeholder="请输入煤炭编号"
+              v-model="input">
+            </el-input>
+            <el-button  style="height: 36px;width: 60px" type="primary" icon="el-icon-search">搜索</el-button>
+            </div>
           <el-table
             :data="tableData"
             height="500"
@@ -93,15 +107,17 @@
       </div>
     </el-breadcrumb>
   </div>
-  </body>
+</el-card>
 </template>
 
 <script>
+
 import * as echarts from 'echarts';
 export default {
   name: "analyse",
   data() {
     return {
+      input:'',
       tableData: [{
         number: '1',
         id: 'C2023092503',
@@ -225,48 +241,48 @@ export default {
       },{
         number: '9',
         id: 'C2023101903',
-        shuifen1: '8.88',
-        shuifen2: '9.13',
-        huifen1: '22.44',
-        huifen2: '23.11',
-        midu1: '4.23',
-        midu2: '3.79',
-        huifafen1: '28.21',
-        huifafen2: '29.79',
-        dianzulv1: '3.78',
-        dianzulv2: '4.22',
-        jidi1: '49.17',
-        jidi2: '49.17',
+        shuifen1: '8.28',
+        shuifen2: '9.01',
+        huifen1: '23.14',
+        huifen2: '23.02',
+        midu1: '4.33',
+        midu2: '4.09',
+        huifafen1: '28.11',
+        huifafen2: '29.99',
+        dianzulv1: '3.71',
+        dianzulv2: '4.21',
+        jidi1: '49.36',
+        jidi2: '49.49',
       },{
         number: '10',
         id: 'C2023101904',
-        shuifen1: '8.88',
-        shuifen2: '9.13',
-        huifen1: '22.44',
-        huifen2: '23.11',
-        midu1: '4.23',
-        midu2: '3.79',
-        huifafen1: '28.21',
-        huifafen2: '29.79',
-        dianzulv1: '3.78',
-        dianzulv2: '4.22',
-        jidi1: '49.17',
-        jidi2: '49.17',
+        shuifen1: '8.97',
+        shuifen2: '9.03',
+        huifen1: '22.41',
+        huifen2: '23.61',
+        midu1: '4.33',
+        midu2: '3.89',
+        huifafen1: '28.97',
+        huifafen2: '29.09',
+        dianzulv1: '3.28',
+        dianzulv2: '3.79',
+        jidi1: '50.17',
+        jidi2: '50.37',
       },{
         number: '11',
         id: 'C2023102105',
-        shuifen1: '8.88',
-        shuifen2: '9.13',
-        huifen1: '22.44',
-        huifen2: '23.11',
-        midu1: '4.23',
-        midu2: '3.79',
-        huifafen1: '28.21',
-        huifafen2: '29.79',
-        dianzulv1: '3.78',
-        dianzulv2: '4.22',
-        jidi1: '49.17',
-        jidi2: '49.17',
+        shuifen1: '9.38',
+        shuifen2: '9.63',
+        huifen1: '23.44',
+        huifen2: '23.51',
+        midu1: '4.03',
+        midu2: '4.19',
+        huifafen1: '29.11',
+        huifafen2: '29.06',
+        dianzulv1: '3.99',
+        dianzulv2: '4.02',
+        jidi1: '50.67',
+        jidi2: '50.37',
       },]
     }
   },
@@ -406,12 +422,35 @@ export default {
             {
               name:'第一次检测',
               type:'bar',
-              data:[8.82,23.28,3.97,28.30,3.77,49.99,8.62,23.15,3.85,28.60,3.86,50.68,9.18,21.61,3.76,28.05,3.95,49.61,8.83,23.21,3.87,28.11,4.02,50.53,8.12,],
+              data:[8.82,23.28,3.97,28.30,3.77,49.99,
+                8.62,23.15,3.85,28.60,3.86,50.68,
+                9.18,21.61,3.76,28.05,3.95,49.61,
+                8.83,23.21,3.87,28.11,4.02,50.53,
+                8.12,22.77,4.31,28.39,4.06,49.44,
+                8.13,22.34,4.21,28.33,3.88,49.27,
+                8.19,22.21,4.11,28.36,3.89,49.29,
+                8.21,22.04,4.01,28.03,3.98,49.77,
+                8.28,23.14,4.33,28.11,3.71,49.36,
+                8.97,22.41,4.33,28.97,3.28,50.17,
+                9.38,23.44,4.03,29.11,3.99,50.67,
+              ],
             },
             {
               name:'第二次检测',
               type:'bar',
-              data:[8.84,22.61,1.4,38.31,4,29.27,8.84,22.61,1.4,38.31,4,29.27],
+              data:[8.48,23.92,4.13,28.11,3.81,49.35,
+              8.43,21.50,3.93,28.33,3.93,50.26,
+              9.38,21.51,4.05,28.81,4.09,51.35,
+              8.14,22.82,4.10,28.90,4.05,48.36,
+              8.36,22.66,3.86,29.43,4.04,50.21,
+              9.32,23.01,3.99,29.03,4.12,49.36,
+              9.22,22.91,3.89,29.01,4.10,49.46,
+              9.02,23.03,3.79,29.06,4.01,49.86,
+              9.01,23.02,4.09,29.99,3.71,49.49,
+              9.03,23.61,3.89,29.09,3.79,50.37,
+              9.63,23.51,4.19,29.06,4.02,50.37,
+
+              ],
             }
           ]
         };
@@ -427,34 +466,29 @@ export default {
 
 </script>
 <style scoped>
-header {
-  height: 50px;
-  background-size: 100% 100%;
-}
-
-header h1 {
-  position: absolute;
-  top: 10px;
-  left: 10px;
-}
 
 *{
   margin:0;
   padding: 0;
 }
+
 .divBox{
   height: 1200px;
 }
+
 .top{
   position: absolute;
+  top: 50px;
   float:left;
   width:1825px;
   height: 100%;
 }
+
 .bottom{
   position: absolute;
   top:650px;
   height: 100%;
   width: 2000px;
 }
+
 </style>
