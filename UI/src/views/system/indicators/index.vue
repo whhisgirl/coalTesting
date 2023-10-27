281530cd748329c3a1e2a97d79a75ef346766137
yyl0719 <1871253922@qq.com>
2023-10-27 15:57:15 +0800

    指标记录

--- UI/src/views/system/indicators/index.vue
+++ UI/src/views/system/indicators/index.vue
@@ -59,7 +59,7 @@
       </el-form-item>
       <el-form-item>
         <el-button type="primary" icon="el-icon-search" size="mini" @click="handleQuery">搜索</el-button>
-        <el-button icon="el-icon-refresh" size="mini" @click="resetQuery">重置</el-button>
+        <el-button  icon="el-icon-refresh" size="mini" @click="resetQuery">重置</el-button>
       </el-form-item>
     </el-form>
     <el-dialog :title="title" :visible.sync="open1" width="1200px" append-to-body>
