fbc59319a52806b50ca0c53ee130f0d680562ea2
lxpeak <742907278@qq.com>
2023-10-27 15:57:30 +0800

    煤质异常情况图表

--- UI/src/views/coalTransportationAnalysis/index.vue
+++ UI/src/views/coalTransportationAnalysis/index.vue
@@ -1,30 +1,31 @@
 <template>
   <div>
     <el-card class="box-card">
-      <el-form ref="form" :model="form" label-width="80px">
+      <el-form ref="searchForm" :model="searchForm" label-width="80px">
         <el-row>
           <el-col :span="4">
             <el-form-item label="批次编号">
-              <el-input v-model="form.batchNumber"></el-input>
-            </el-form-item>
-          </el-col>
-          <el-col :span="7">
-            <el-form-item label="检测时间">
-              <el-date-picker v-model="form.sampleTimeList" type="daterange" range-separator="至" start-placeholder="开始日期"
-                end-placeholder="结束日期" value-format="yyyy-MM-dd">
-              </el-date-picker>
+              <el-input v-model="searchForm.batchNumber" label-width="auto"></el-input>
             </el-form-item>
           </el-col>
           <el-col :span="4">
             <el-form-item label="车牌号">
-              <el-input v-model="form.licensePlate"></el-input>
+              <el-input v-model="searchForm.licensePlate" label-width="auto"></el-input>
             </el-form-item>
           </el-col>
+          <el-col :span="8">
+            <el-form-item label="检测时间">
+              <el-date-picker v-model="sampleTimeList" type="daterange" range-separator="至" start-placeholder="开始日期"
+                end-placeholder="结束日期" value-format="yyyy-MM-dd" label-width="auto">
+              </el-date-picker>
+            </el-form-item>
+          </el-col>
+
           <el-col :span="6">
             <el-form-item label="所在矿区">
-              <el-select v-model="form.miningAreaName" placeholder="请选择矿区">
-                <el-option label="A矿区" value="A"></el-option>
-                <el-option label="B矿区" value="B"></el-option>
+              <el-select v-model="searchForm.miningAreaName" placeholder="请选择矿区">
+                <el-option label="大同矿区" value="大同矿区"></el-option>
+                <el-option label="忻州矿区" value="忻州矿区"></el-option>
               </el-select>
             </el-form-item>
           </el-col>
@@ -68,33 +69,153 @@
         @pagination="getList" />
 
       <!-- 对话框 -->
-      <el-dialog :title="title" :visible.sync="open" width="80%" append-to-body>
-        <el-form ref="form" :model="form" :rules="rules" label-width="80px">
+      <el-dialog :title="title" :visible.sync="open" width="90%" style="padding-bottom: 0;" append-to-body>
+        <el-form ref="infoForm" :model="infoForm" :rules="rules" label-width="80px">
           <el-row>
-            <el-col :span="12">
-              <el-form-item label="批次编号" prop="registrationNumber">
-                {{ 1 }}
+            <el-col :span="4" :offset="1">
+              <el-form-item label="批次编号:" prop="batchNumber" label-width="auto">
+                {{ infoForm.batchNumber }}
               </el-form-item>
             </el-col>
-            <el-col :span="12">
-              <el-form-item label="矿区名称" prop="miningAreaName">
-                {{ 'a' }}
+            <el-col :span="4">
+              <el-form-item label="矿区名称:" prop="miningAreaName" label-width="auto">
+                {{ infoForm.miningAreaName }}
+              </el-form-item>
+            </el-col>
+            <el-col :span="6">
+              <el-form-item label="运输车辆车牌:" prop="licensePlate" label-width="auto">
+                {{ infoForm.licensePlate }}
+              </el-form-item>
+            </el-col>
+            <el-col :span="6">
+              <el-form-item label="矿区所在地:" prop="miningAreaName" label-width="auto">
+                {{ infoForm.miningAreaName }}
+              </el-form-item>
+            </el-col>
+          </el-row>
+          <el-divider></el-divider>
+          <el-row>
+            <el-col :span="3" :offset="1">
+              <el-form-item label="运输前水分:" label-width="auto">
+                {{ infoForm.firstWater }}
+              </el-form-item>
+            </el-col>
+            <el-col :span="3">
+              <el-form-item label="运输前灰分:" label-width="auto">
+                {{ infoForm.firstAshContent }}
+              </el-form-item>
+            </el-col>
+            <el-col :span="3">
+              <el-form-item label="运输前密度:" label-width="auto">
+                {{ infoForm.firstDensity }}
+              </el-form-item>
+            </el-col>
+            <el-col :span="3">
+              <el-form-item label="运输前挥发分:" label-width="auto">
+                {{ infoForm.firstCoalVolatile }}
+              </el-form-item>
+            </el-col>
+            <el-col :span="3">
+              <el-form-item label="运输前电阻率:" label-width="auto">
+                {{ infoForm.firstrResistivity }}
+              </el-form-item>
+            </el-col>
+            <el-col :span="3">
+              <el-form-item label="运输前基低位发热量:" label-width="auto">
+                {{ infoForm.firstBaseLowCalorific }}
               </el-form-item>
             </el-col>
           </el-row>
           <el-row>
-            <el-col :span="12">
-              <el-form-item label="运输车辆车牌" prop="licensePlate">
-                {{ 'ASA' }}
+            <el-col :span="3" :offset="1">
+              <el-form-item label="运输后水分:" label-width="auto">
+                {{ infoForm.secondWater }}
               </el-form-item>
             </el-col>
-            <el-col :span="12">
-              <el-form-item label="矿区所在地" prop="locationMiningArea">
-                {{ 'AADD' }}
+            <el-col :span="3">
+              <el-form-item label="运输后灰分:" label-width="auto">
+                {{ infoForm.secondAshContent }}
+              </el-form-item>
+            </el-col>
+            <el-col :span="3">
+              <el-form-item label="运输后密度:" label-width="auto">
+                {{ infoForm.secondDensity }}
+              </el-form-item>
+            </el-col>
+            <el-col :span="3">
+              <el-form-item label="运输后挥发分:" label-width="auto">
+                {{ infoForm.secondCoalVolatile }}
+              </el-form-item>
+            </el-col>
+            <el-col :span="3">
+              <el-form-item label="运输后电阻率:" label-width="auto">
+                {{ infoForm.secondrResistivity }}
+              </el-form-item>
+            </el-col>
+            <el-col :span="3">
+              <el-form-item label="运输后基低位发热量:" label-width="auto">
+                {{ infoForm.secondBaseLowCalorific }}
               </el-form-item>
             </el-col>
           </el-row>
         </el-form>
+
+        <!-- <el-descriptions class="margin-top" title="带边框列表" :column="3" :size="size" border>
+          <el-descriptions-item>
+            <template slot="label">
+              <i class="el-icon-user"></i>
+              批次编号
+            </template>
+            kooriookami
+          </el-descriptions-item>
+          <el-descriptions-item>
+            <template slot="label">
+              <i class="el-icon-mobile-phone"></i>
+              矿区名称
+            </template>
+            18100000000
+          </el-descriptions-item>
+          <el-descriptions-item>
+            <template slot="label">
+              <i class="el-icon-location-outline"></i>
+              运输车辆车牌
+            </template>
+            苏州市
+          </el-descriptions-item>
+          <el-descriptions-item>
+            <template slot="label">
+              <i class="el-icon-tickets"></i>
+              矿区所在地
+            </template>
+            <el-tag size="small">学校</el-tag>
+          </el-descriptions-item>
+        </el-descriptions> -->
+
+        <div>
+          <div style="width: 100%; background: #d2e9ff; border-radius: 10px">
+            <p style="
+            font-family: Arial;
+            font-size: 16px;
+            font-weight: 600;
+            display: inline-block;
+            margin-left: 20px;
+          ">
+              异常批次详情
+            </p>
+            <el-tooltip placement="top">
+              <div slot="content">
+                <p>水分(0~1%)</p>
+                <p>灰分(0~1%)</p>
+                <p>密度(0~0.5%)</p>
+                <p>挥发分(0~1%)</p>
+                <p>电阻率(0~1%)</p>
+                <p>发热量(0~1.5%)</p>
+              </div>
+              <i class="el-icon-question"
+                style="float: right; margin-right: 20px; margin-top: 12px; font-size: 30px;"></i>
+            </el-tooltip>
+          </div>
+        </div>
         <div ref="exceptionCompareChart" style="width: 100%;height: 300px;"></div>
         <div slot="footer" class="dialog-footer">
           <el-button @click="cancel">退 出</el-button>
@@ -111,17 +232,6 @@
           ">
           煤质异常次数
         </p>
-        <el-tooltip placement="top">
-          <div slot="content">
-            <p>水分(0~1%)</p>
-            <p>灰分(0~1%)</p>
-            <p>密度(0~0.5%)</p>
-            <p>挥发分(0~1%)</p>
-            <p>电阻率(0~1%)</p>
-            <p>发热量(0~1.5%)</p>
-          </div>
-          <i class="el-icon-question" style="float: right; margin-right: 20px; margin-top: 12px; font-size: 30px;"></i>
-        </el-tooltip>
       </div>
       <div style="width: 100%;height: 200px;" ref="exceptionBarChart"></div>
     </el-card>
@@ -137,11 +247,13 @@ export default {
   name: "coalTransportationAnalysis",
   data() {
     return {
-      form: {
+      searchForm: {
         pageNum: 1,
         pageSize: 10,
-        sampleTimelist:[],
+        miningAreaName: ''
       },
+      sampleTimeList: [],
+      infoForm: {},
       // 遮罩层
       loading: false,
       // 选中数组
@@ -154,8 +266,11 @@ export default {
       showSearch: true,
       // 总条数
       total: 0,
-      // 【请填写功能名称】表格数据
+      // 异常批次图表
       exceptionList: [],
+
+      // 异常批次对比
+      exceptionCompareList: [],
 
       // 弹出层标题
       title: "",
@@ -169,18 +284,7 @@ export default {
       //表单校验
       rules: {},
       chartData1: [
-        {
-          value: 3.6,
-          itemStyle: {
-            color: '#a90000'
-          }
-        }, 0.7, 0.4, 0.6, 0.4,
-        {
-          value: -2.3,
-          itemStyle: {
-            color: '#a90000'
-          }
-        }
+
       ],
       chartData2: [
         0.7, 0.4, {
@@ -208,22 +312,38 @@ export default {
   },
   methods: {
     search() {
-      console.log(this.form);
-      this.form.sampleTimeBegin = this.form.sampleTimeList[0]
-      this.form.sampleTimeAfter = this.form.sampleTimeList[1]
-      listBatchAndIndicators(this.form).then(response => {
+      console.log(this.searchForm);
+      if (this.sampleTimeList !== null) {
+        this.searchForm.sampleTimeBegin = this.sampleTimeList[0]
+        this.searchForm.sampleTimeAfter = this.sampleTimeList[1]
+      }
+      listBatchAndIndicators(this.searchForm).then(response => {
         console.log(response);
-        this.exceptionList = response.data.batchAndIndicatorsList
+        let list = response.data.batchAndIndicatorsList
+        //去重
+        const filterList = (arr, id) => {
+          let newList = arr.reduce((pre, cur) => pre.some(item => item[id] === cur[id]) ? pre : [...pre, cur], [])
+          return newList;
+        }
+        this.exceptionList = filterList(list, 'batchNumber')
         this.total = response.data.total
       });
     },
     cancel() {
       this.open = false
     },
-    reset(){
-      listBatchAndIndicators({pageNum: 1, pageSize: 10,}).then(response => {
+    reset() {
+      this.searchForm = {}
+      this.sampleTimeList = []
+      listBatchAndIndicators({ pageNum: 1, pageSize: 10, }).then(response => {
         console.log(response);
-        this.exceptionList = response.data.batchAndIndicatorsList
+        let list = response.data.batchAndIndicatorsList
+        //去重
+        const filterList = (arr, id) => {
+          let newList = arr.reduce((pre, cur) => pre.some(item => item[id] === cur[id]) ? pre : [...pre, cur], [])
+          return newList;
+        }
+        this.exceptionList = filterList(list, 'batchNumber')
         this.total = response.data.total
       });
     },
@@ -232,15 +352,156 @@ export default {
     getList() {
       listBatchAndIndicators(this.queryParams).then(response => {
         console.log(response);
-        this.exceptionList = response.data.batchAndIndicatorsList
+        let list = response.data.batchAndIndicatorsList
+        //去重
+        const filterList = (arr, id) => {
+          let newList = arr.reduce((pre, cur) => pre.some(item => item[id] === cur[id]) ? pre : [...pre, cur], [])
+          return newList;
+        }
+        this.exceptionList = filterList(list, 'batchNumber')
         this.total = response.data.total
       });
     },
     handleInfo(row) {
-      this.open = true
-      this.$nextTick(()=>{
-        this.initExceptionCompareChart()
-      })
+      this.title = "异常批次详情"
+      console.log(row);
+      listBatchAndIndicators({ coalNumber: row.coalNumber }).then(response => {
+        console.log(response);
+        this.exceptionCompareList = response.data.batchAndIndicatorsList
+        this.total = response.data.total
+        this.infoForm.batchNumber = this.exceptionCompareList[0].batchNumber
+        this.infoForm.miningAreaName = this.exceptionCompareList[0].miningAreaName
+        this.infoForm.licensePlate = this.exceptionCompareList[0].licensePlate
+        this.infoForm.miningAreaName = this.exceptionCompareList[0].miningAreaName
+
+        this.infoForm.firstWater = this.exceptionCompareList[0].waterContent
+        this.infoForm.firstAshContent = this.exceptionCompareList[0].ashContent
+        this.infoForm.firstBaseLowCalorific = this.exceptionCompareList[0].baseLowCalorific
+        this.infoForm.firstCoalVolatile = this.exceptionCompareList[0].coalVolatile
+        this.infoForm.firstDensity = this.exceptionCompareList[0].density
+        this.infoForm.firstrResistivity = this.exceptionCompareList[0].resistivity
+
+        this.infoForm.secondWater = this.exceptionCompareList[1].waterContent
+        this.infoForm.secondAshContent = this.exceptionCompareList[1].ashContent
+        this.infoForm.secondBaseLowCalorific = this.exceptionCompareList[1].baseLowCalorific
+        this.infoForm.secondCoalVolatile = this.exceptionCompareList[1].coalVolatile
+        this.infoForm.secondDensity = this.exceptionCompareList[1].density
+        this.infoForm.secondrResistivity = this.exceptionCompareList[1].resistivity
+
+        //水分
+        let waterContentDiff = ((this.exceptionCompareList[0].waterContent - this.exceptionCompareList[1].waterContent) / this.exceptionCompareList[0].waterContent).toFixed(5)
+        if (Math.abs(waterContentDiff) > 0.01) {
+          this.chartData1.push({
+            value: waterContentDiff,
+            itemStyle: {
+              color: '#a90000'
+            }
+          })
+        } else {
+          this.chartData1.push({
+            value: waterContentDiff,
+            itemStyle: {
+              color: '#5470C6'
+            }
+          })
+        }
+
+        //灰分
+        let ashContentDiff = ((this.exceptionCompareList[1].ashContent - this.exceptionCompareList[0].ashContent) / this.exceptionCompareList[0].ashContent).toFixed(5)
+        if (Math.abs(ashContentDiff) > 0.01) {
+          this.chartData1.push({
+            value: ashContentDiff,
+            itemStyle: {
+              color: '#a90000'
+            }
+          })
+        } else {
+          this.chartData1.push({
+            value: ashContentDiff,
+            itemStyle: {
+              color: '#5470C6'
+            }
+          })
+        }
+        console.log("ashContentDiff", ashContentDiff);
+
+        //密度
+        let densityDiff = ((this.exceptionCompareList[1].density - this.exceptionCompareList[0].density) / this.exceptionCompareList[0].density).toFixed(5)
+        if (Math.abs(densityDiff) > 0.005) {
+          this.chartData1.push({
+            value: densityDiff,
+            itemStyle: {
+              color: '#a90000'
+            }
+          })
+        } else {
+          this.chartData1.push({
+            value: densityDiff,
+            itemStyle: {
+              color: '#5470C6'
+            }
+          })
+        }
+
+        //挥发分
+        let coalVolatileDiff = ((this.exceptionCompareList[1].coalVolatile - this.exceptionCompareList[0].coalVolatile) / this.exceptionCompareList[0].coalVolatile).toFixed(5)
+        if (Math.abs(coalVolatileDiff) > 0.01) {
+          this.chartData1.push({
+            value: coalVolatileDiff,
+            itemStyle: {
+              color: '#a90000'
+            }
+          })
+        } else {
+          this.chartData1.push({
+            value: coalVolatileDiff,
+            itemStyle: {
+              color: '#5470C6'
+            }
+          })
+        }
+
+        //电阻率
+        let resistivityDiff = ((this.exceptionCompareList[1].resistivity - this.exceptionCompareList[0].resistivity) / this.exceptionCompareList[0].resistivity).toFixed(5)
+        if (Math.abs(resistivityDiff) > 0.01) {
+          this.chartData1.push({
+            value: resistivityDiff,
+            itemStyle: {
+              color: '#a90000'
+            }
+          })
+        } else {
+          this.chartData1.push({
+            value: resistivityDiff,
+            itemStyle: {
+              color: '#5470C6'
+            }
+          })
+        }
+
+        //基低位发热量
+        let baseLowCalorificDiff = ((this.exceptionCompareList[1].baseLowCalorific - this.exceptionCompareList[0].baseLowCalorific) / this.exceptionCompareList[0].baseLowCalorific).toFixed(5)
+        if (Math.abs(baseLowCalorificDiff) > 0.015) {
+          this.chartData1.push({
+            value: baseLowCalorificDiff,
+            itemStyle: {
+              color: '#a90000'
+            }
+          })
+        } else {
+          this.chartData1.push({
+            value: baseLowCalorificDiff,
+            itemStyle: {
+              color: '#5470C6'
+            }
+          })
+        }
+        this.open = true
+        this.$nextTick(() => {
+          this.initExceptionCompareChart()
+        })
+      });
+
     },
     initExceptionNumBarChart() {
       this.exceptionBarChart = echarts.init(this.$refs.exceptionBarChart);
@@ -292,7 +553,11 @@ export default {
         series: [
           {
             data: this.chartData1,
-            type: 'bar'
+            type: 'bar',
+            label: {
+              show: true,
+              position: 'inside'
+            },
           },
         ]
       }
