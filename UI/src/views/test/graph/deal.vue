2d223efa19241ab3a9b3333ef03fdc0e4f63f0ff
meiyoutishi <1501935525@qq.com>
2023-12-04 09:58:54 +0800

    知识图谱

---
+++ UI/src/views/test/graph/deal.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+<el-col :span="12">
+    <one-radical></one-radical>
+  <span></span><el-button type="primary" class="searchButton">搜索</el-button>
+</el-col>
+    <el-col  :span="12">
+
+    </el-col>
+  </div>
+</template>
+
+<script>
+
+import scrollNumber from "@/components/scrollNumber/scrollNumber.vue";
+import center from "@/views/algorithm/gundong.vue";
+import oneRadical from "./oneRadical.vue";
+export default {
+  name: "Graph",
+  components:{
+    // right2
+    oneRadical,
+  },
+
+
+
+}
+</script>
+
+<style scoped>
+.root {
+  height: calc(90vh - 120px);
+  width: 100%;
+}
+.searchButton{
+  height:50px;
+  width:100px;
+  font-size:18px;
+  float:right
+}
+.graph {
+  height: 100%;
+  width: 100%;
+  /*border: 1px solid black*/
+  /*display: inline-block;*/
+  /*position: relative;*/
+  /*top: -105%;*/
+  /*right: -40%*/
+}
+</style>
+
--- UI/src/views/test/graph/map.vue
+++ UI/src/views/test/graph/map.vue
@@ -7,7 +7,21 @@ export default {
   data() {
     return {
       position_NKYYGS: {
-        Name: '忻州市',
+        Name: '忻州宏远矿区',
+        address: '',
+        phoneNumber: '',
+        bdLNG: 112.74286015132837,
+        bdLAT: 38.42148871204489
+      },
+      position_NKYYDS: {
+        Name: '红池矿区',
+        address: '',
+        phoneNumber: '',
+        bdLNG: 112.74286015132837,
+        bdLAT: 38.42148871204489
+      },
+      position_NKYYFS: {
+        Name: '南岭矿区',
         address: '',
         phoneNumber: '',
         bdLNG: 112.74286015132837,
@@ -51,6 +65,48 @@ export default {
       var infoWindow = new BMap.InfoWindow(sContent);
       marker.addEventListener('click', function() {
         this.openInfoWindow(infoWindow);
+      });
+      var sContent2 =
+        '<div style="width:300px;"><p style="font-size:16px;font-weight:bold;margin-top: 10px;color:#D07852">' +
+        this.position_NKYYDS.Name +
+        '</p><p style="font-size:13px;margin: 5px 0;">地址：' +
+        this.position_NKYYDS.address +
+        '</p><p style="font-size:13px;margin: 5px 0;">电话：' +
+        this.position_NKYYDS.phoneNumber +
+        '</p><p style="font-size:13px;margin: 5px 0;">坐标：' +
+        this.position_NKYYDS.bdLNG +
+        ',' +
+        this.position_NKYYDS.bdLAT;
+      ('</div>');
+      var point2 = new BMap.Point(this.position_NKYYDS.bdLNG, this.position_NKYYDS.bdLAT); // 创建点坐标
+      var marker2 = new BMap.Marker(point2);
+      map.addOverlay(marker2);
+      map.centerAndZoom(point2, 10); // 初始化地图，设置中心点坐标和地图级别
+      // //点击图标时候调用信息窗口
+      var infoWindow2 = new BMap.InfoWindow(sContent2);
+      marker.addEventListener('click', function() {
+        this.openInfoWindow(infoWindow2);
+      });
+      var sContent3 =
+        '<div style="width:300px;"><p style="font-size:16px;font-weight:bold;margin-top: 10px;color:#D07852">' +
+        this.position_NKYYFS.Name +
+        '</p><p style="font-size:13px;margin: 5px 0;">地址：' +
+        this.position_NKYYFS.address +
+        '</p><p style="font-size:13px;margin: 5px 0;">电话：' +
+        this.position_NKYYFS.phoneNumber +
+        '</p><p style="font-size:13px;margin: 5px 0;">坐标：' +
+        this.position_NKYYFS.bdLNG +
+        ',' +
+        this.position_NKYYFS.bdLAT;
+      ('</div>');
+      var point3 = new BMap.Point(this.position_NKYYFS.bdLNG, this.position_NKYYFS.bdLAT); // 创建点坐标
+      var marker3 = new BMap.Marker(point3);
+      map.addOverlay(marker3);
+      map.centerAndZoom(point3, 10); // 初始化地图，设置中心点坐标和地图级别
+      // //点击图标时候调用信息窗口
+      var infoWindow3 = new BMap.InfoWindow(sContent3);
+      marker.addEventListener('click', function() {
+        this.openInfoWindow(infoWindow3);
       });
     }
   }
